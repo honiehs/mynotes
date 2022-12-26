168cac3cc8d761b32d301f5f27e0029220868934
honieh <honiehsadii@gmail.com>
2022-12-26 18:31:39 +0330

    step-11-crud

---
+++ lib/services/crud/crud_exceptions.dart
@@ -0,0 +1,17 @@
+class DatabaseAlreadyOpenException implements Exception {}
+
+class UnableToGetDocumentDirectory implements Exception {}
+
+class DatabaseIsNoteOpen implements Exception {}
+
+class CouldNotDeleteUser implements Exception {}
+
+class UserAlreadyExists implements Exception {}
+
+class CouldNotFindUser implements Exception {}
+
+class CouldNotDeleteNote implements Exception {}
+
+class CouldNotFindNotes implements Exception {}
+
+class CouldNotUpdatenote implements Exception {}
